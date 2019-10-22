f1d92adaa5b50372335086303ed594db4d377d19
Marta Suska <mr.ska@wp.pl>
2019-10-22 20:26:42 +0200

    Examples from the second lesson

---
+++ lessons/day02.py
@@ -0,0 +1,37 @@
+# suma = 2+2
+# print(suma)
+# print(2+2)
+#
+# # better to use first option
+# # python needs a new line at the end of the file, pep8
+#
+# print("###########################################################################")
+#
+# # KALKULATOR
+# #first_number = int(input('First number: '))
+# #second_number = int(input('Second number: '))
+#
+# #print(first_number+second_number)
+#
+# print("###########################################################################")
+#
+tekst = "ala ma kota"
+# ulamek = 1.3
+# ilosc_powtorzen = 5
+#
+# print(tekst)
+# print(tekst.capitalize())
+# print(tekst.upper())
+
+# print("###########################################################################")
+
+tekst = "ala ma kota"
+len_tekst = len(tekst)
+print(tekst[len_tekst-1])
+
+
+# print("###########################################################################")
+
+exit()
+
+
