be558b951a3c008d618d55ce4ff6b94318c90f98
Marta Suska <mr.ska@wp.pl>
2019-10-29 16:38:09 +0100

    Program 6 - dodana opcja z listami zagniezdzonymi

--- homeworks/day3/program6.py
+++ homeworks/day3/program6.py
@@ -1,37 +1,59 @@
 # na podstawie https://pynative.com/python-accept-list-input-from-user/
 
-listaUzytkownika = input("Wpisz elementy listy rozdzielone spacja ")
-lista  = listaUzytkownika.split()
-dlugoscListy = len(lista)
+listaUzytkownika = input("Wpisz elementy listy rozdzielone spacja, a listy zagnieżdżone rozdzielone \'-\' ")  # np aaaa-a a-b-c aa1  1-2
+lista1  = listaUzytkownika.split()
+lista2 = list()
 
+for i in range(0,len(lista1)):
+   listaTemp = lista1[i].split("-")
+   lista2.append(listaTemp)
+
+# ustalenie liczby wierszy
+liczbaWierszy = len(lista2)
+
+# ustalenie liczby kolumn
+liczbaKolumn = 0
+for i in lista2:
+    szerokoscTemp = len(i)
+    if szerokoscTemp > liczbaKolumn:
+        liczbaKolumn = szerokoscTemp
+
+# dopisanie elementow do krotszych list
+for i in range(0, len(lista2)):
+    szerokoscTemp = len(lista2[i])
+    while szerokoscTemp < liczbaKolumn:
+        lista2[i].append("")
+        szerokoscTemp = len(lista2[i])
 # ustalenie maksymalnej szerokosc kolumny
-szerokosc = 1
+szerokoscKolumn = 1
+for i in range(0, len(lista2)):
+    for j in range(0, len(lista2[i])):
+        if len(lista2[i][j]) > 30:
+            lista2[i][j] = lista2[i][j][0:27] + "..."
 
-licznik = 0
-for i in lista:
-    if len(i) > 30:
-        lista[licznik] = i[0:27] +  "..."
-    licznik = licznik + 1
+        szerokosc = len(lista2[i][j])
 
-for i in lista:
-    if len(i) > szerokosc:
-        szerokosc = len(i)
+        if szerokosc > szerokoscKolumn:
+            szerokoscKolumn = szerokosc
 
-# ustalenie ilosci spacji dla kazdego elementu listy
+# dodanie spacji, zeby kazd akolumna byla tej samej szerokosci
 liczbaSpacji = list()
-for i in lista:
-    liczbaSpacji.append(szerokosc - len(i))
+for i in range(0, len(lista2)):
+    for j in range(0, len(lista2[i])):
+        while szerokoscKolumn > len(lista2[i][j]):
+            lista2[i][j] = lista2[i][j]+' '
 
-# dodanie marginesow
-szerokosc = szerokosc + 2
+#dodanie marginesow
+szerokoscKolumn = szerokoscKolumn + 2
 
-linia = ("+" + "-" * szerokosc) * dlugoscListy + "+\n" + "|"
-for i in range(0, dlugoscListy):
-    linia = linia + " " + lista[i] + liczbaSpacji[i] * " " + " |"
+linia = ""
+licznik = 0
+for i in range(0, len(lista2)):
+    linia = linia + ("+" + "-" * szerokoscKolumn) * liczbaKolumn + "+\n"
+    for j in range(0, len(lista2[i])):
+        linia = linia + "| " + lista2[i][j] + " "
+    linia = linia + "|\n"
 
-linia = linia + "\n" + ("+" + "-" * szerokosc) * dlugoscListy + "+\n"
+linia = linia + ("+" + "-" * szerokoscKolumn) * liczbaKolumn + "+\n"
+
 print(linia)
-
-
-
-
