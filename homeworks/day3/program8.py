57b67501c12b7aeae8a1f66871d5d1eaf3320cd1
Marta Suska <mr.ska@wp.pl>
2019-10-29 08:34:29 +0100

    Program 8

---
+++ homeworks/day3/program8.py
@@ -0,0 +1,4 @@
+wysokoscPiramidy  = int(input("Podaj wysokość piramidy: "))
+
+for i in range(0, wysokoscPiramidy):
+    print((wysokoscPiramidy - i - 1) * " " + (i * 2 + 1) * "#" + (wysokoscPiramidy - i - 1) * " ")
