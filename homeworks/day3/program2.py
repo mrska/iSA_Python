31d54af7ef3a20547dc8090554c1621761099e63
Marta Suska <mr.ska@wp.pl>
2019-12-01 16:13:53 +0100

    day3
    porzadek w notatkach

--- homeworks/day3/program2.py
+++
@@ -1,8 +0,0 @@
-# Wypisanie pierwszej i ostatniej cyfry podanej liczby
-
-number = input("Podaj liczbę: ")
-
-print("Pierwsza cyfra liczby " + number + " to " + number[0])
-print("Ostatnia cyfra liczby " + number + " to " + number[-1])
-
-# Dodac sprawdzenie czy podano liczbe, uwzglednic ujemne
